7098b0b62cf23f114b4c99c1a254110c11becf6e
Sora Morimoto <sora@morimoto.io>
2024-12-06 04:24:04 +0900

    Cleanup
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,16 @@
 import * as process from "node:process";
-
 import rescript from "@jihchi/vite-plugin-rescript";
 import { defineConfig } from "vite";
-import { ViteEjsPlugin as ejs } from "vite-plugin-ejs";
-
-import { dependencies } from "./package.json";
-
-function createManualChunks(
-  deps: Record<string, string>,
-): Record<string, string[]> {
-  const ignore = new Set(["react", "react-dom", "modern-css-reset"]);
-  const chunks = Object.keys(deps)
-    .filter((dep) => !ignore.has(dep))
-    .reduce(
-      (acc, dep) => {
-        acc[dep] = [dep];
-        return acc;
-      },
-      {} as Record<string, string[]>,
-    );
-  return chunks;
-}
 
 export default defineConfig({
   base: process.env.CI ? "/rescript-videojs-example/" : "/",
   build: {
     cssMinify: "lightningcss",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          ...createManualChunks(dependencies),
-        },
-      },
-    },
-    target: "esnext",
   },
   css: {
     transformer: "lightningcss",
   },
-  plugins: [rescript({ silent: true }), ejs()],
+  plugins: [rescript({ silent: true })],
   server: {
     port: 3000,
   },
