347df59ccbd8a4599ddaa4028b1ced8be8914b80
Sora Morimoto <sora@morimoto.io>
2025-03-03 05:23:59 +0900

    React Scan
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import * as process from "node:process";
 import rescript from "@jihchi/vite-plugin-rescript";
+import reactScan from "@react-scan/vite-plugin-react-scan";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: process.env.CI ? "/rescript-videojs-example/" : "/",
-  plugins: [tailwindcss(), rescript({ silent: true })],
+  plugins: [tailwindcss(), rescript({ silent: true }), reactScan()],
   server: {
     port: 3000,
   },
