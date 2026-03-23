e469f70a8733c30efa3d6a6b7a365e298bbef94a
Sora Morimoto <sora@morimoto.io>
2026-03-24 05:10:06 +0900

    Replace Vite and Biome with Vite+

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import * as process from "node:process";
 import rescript from "@jihchi/vite-plugin-rescript";
 import reactScan from "@react-scan/vite-plugin-react-scan";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 
 export default defineConfig({
   base: process.env.CI ? "/rescript-videojs-example/" : "/",
   plugins: [tailwindcss(), rescript({ silent: true }), reactScan()],
   server: {
     port: 3000,
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
   },
 });
